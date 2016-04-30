ba76ebf52992e4f8936c98571b5a706c0b47c2f2
Nikita Panin <iamnp@yandex.ru>
2016-04-30 13:19:36 +0300

    Improved examples

---
+++ Examples/AtomModel.cs
@@ -0,0 +1,39 @@
+﻿[MainClass]
+class Game
+{
+  [StateField]
+  double[] angle;
+  
+  [InitMethod]
+  void Init() {
+    angle = new double[5];
+    var r = new Random(99);
+    for (int i = 0; i < 5; ++i) {
+      angle[i] = r.Next(0, 1000) / 10.0;
+    }
+  }
+
+  [TickMethod]
+  void Tick(double dt, Dictionary<char, bool> input) {
+    for (int i = 0; i < 5; ++i) {
+        angle[i] += 0.03*dt;
+    }
+  }
+
+  [DrawMethod]
+  void DrawScene(DrawingContext dc) {
+    dc.Rect(Color.FromArgb(0, 0, 0, 0), 0, 0, 500, 500);
+    dc.Ellipse(Colors.Red, 250, 250, 20, 20);   
+
+    for (int i = 0; i < 5; ++i) {
+       dc.PushTransform(new RotateTransform(36*i, 250, 250));
+       dc.Ellipse(Colors.Gray, 3, 250, 250, 40, 150);
+       dc.Ellipse(Colors.Black,
+          250 + 40*Math.Cos(angle[i]*0.05),
+          250 + 150*Math.Sin(angle[i]*0.05),
+          8, 8);
+       dc.Pop();
+    }
+    
+  }
+}
---
+++ Examples/Orbit.cs
@@ -0,0 +1,37 @@
+﻿[MainClass]
+class Game
+{
+  [StateField]
+  double angle1;
+
+  [TickMethod]
+  void Tick(double dt, Dictionary<char, bool> input) {
+    angle1 += 0.03 * dt;
+  }
+
+  [DrawMethod]
+  void DrawScene(DrawingContext dc) {
+    dc.Rect(Color.FromArgb(255, 0, 0, 30), 0, 0, 500, 500);
+    // sun
+    dc.Ellipse(Colors.Yellow, 250, 250, 30, 30);
+    
+    Planet(dc);
+    
+    Stars(dc);
+  }
+
+  [DrawTrackMethod]
+  void Planet(DrawingContext dc) {
+      dc.Ellipse(Colors.Red,
+          250 + 100*Math.Cos(angle1*0.05),
+          250 + 100*Math.Sin(angle1*0.05),
+          10, 10);
+  }
+  
+  void Stars(DrawingContext dc) {
+    var r = new Random(99);
+    for (int i = 0; i < 50; ++i) {
+      dc.Ellipse(Colors.White, r.Next(0, 501), r.Next(0, 501), 1, 1);
+    }
+  }
+}
